1dd805f33bda31eb10b7347cab1e2cf51ffa7a04
jose.molina <jose.molina@nexcel.cl>
2022-04-27 21:38:30 -0400

    change

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const { config } = require("dotenv")
 config()
 
 module.exports = {
-  PORT: process.env.PORT || 4000,
+  PORT: process.env.PORT || 5000,
   DATABASE_URI: process.env.HEROKU_POSTGRES_URI || "",
   API_VERSION: process.env.API_VERSION || "1.0",
 }
